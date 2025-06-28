a4c90a7b02e3829ffc474b93f31f26fba7f24035
Shreya Tiwari <shreyat1818@gmail.com>
2025-06-28 23:10:07 +0530

    Delete 05-african-cities.sql

--- 05-african-cities.sql
+++
@@ -1,7 +0,0 @@
--- https://www.hackerrank.com/challenges/african-cities/problem
-
-SELECT CITY.NAME
-FROM CITY
-LEFT JOIN COUNTRY
-ON CITY.COUNTRYCODE = COUNTRY.CODE
-WHERE COUNTRY.CONTINENT = 'Africa';
