1b37daf620873757b675a14771debb503b6a1fa6
Shreya Tiwari <shreyat1818@gmail.com>
2025-06-28 23:09:29 +0530

    Delete 01-weather-observation-status-8.sql

--- 01-weather-observation-status-8.sql
+++
@@ -1,6 +0,0 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
-
-SELECT DISTINCT CITY
-FROM STATION
-WHERE LOWER(LEFT(CITY, 1)) IN ('a','e','i','o','u') AND LOWER(RIGHT(CITY, 1))
-IN ('a','e','i','o','u');
